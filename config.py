b4c95520a16d1d8c4136f88ff3a5468d265e9b50
Piyumal98 <59592735+Piyumal98@users.noreply.github.com>
2022-09-27 10:31:06 +0530

    add firebasee as db

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import  pyrebase 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
@@ -7,10 +8,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-        'postgres://ttakpcjxtrnymq:d0c134048277e419f0452f9fa08f1d5e6925f0a0617b69d2a73944bd967163ba@ec2-52-200-5-135.compute-1.amazonaws.com:5432/da8qac8jdae2mc', \
-            'postgresql://ttakpcjxtrnymq:d0c134048277e419f0452f9fa08f1d5e6925f0a0617b69d2a73944bd967163ba@ec2-52-200-5-135.compute-1.amazonaws.com:5432/da8qac8jdae2mc') or \
-        'sqlite:///ttakpcjxtrnymq:d0c134048277e419f0452f9fa08f1d5e6925f0a0617b69d2a73944bd967163ba@ec2-52-200-5-135.compute-1.amazonaws.com:5432/da8qac8jdae2mc' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
